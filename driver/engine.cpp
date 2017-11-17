39b2f2a876946db8426692c5690394dfa3b8924f
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-17 16:40:23 +0800

    RegisterPartitionManager

--- driver/engine.cpp
+++ driver/engine.cpp
@@ -64,49 +64,49 @@ WorkerSpec Engine::AllocateWorkers(const std::vector<WorkerAlloc>& worker_alloc)
 void Engine::InitTable(uint32_t table_id, const std::vector<uint32_t>& worker_ids) {
   // TODO
   CHECK(id_mapper_);
-  std::vector<uint32_t> local_server =id_mapper_->GetServerThreadsForId(node_.id);
-  int count=local_server.size();
-  if(count == 0)
-    return ;
+  std::vector<uint32_t> local_server = id_mapper_->GetServerThreadsForId(node_.id);
+  int count = local_server.size();
+  if (count == 0)
+    return;
   auto id = id_mapper_->AllocateWorkerThread(node_.id);
-  ThreadsafeQueue<Message>queue;
-  mailbox_->RegisterQueue(id,&queue);
+  ThreadsafeQueue<Message> queue;
+  mailbox_->RegisterQueue(id, &queue);
 
   Message reset_msg;
-  reset_msg.meta.flag=Flag::kResetWorkerInModel;
-  reset_msg.meta.model_id=table_id;
-  reset_msg.meta.sender=id;
+  reset_msg.meta.flag = Flag::kResetWorkerInModel;
+  reset_msg.meta.model_id = table_id;
+  reset_msg.meta.sender = id;
   reset_msg.AddData(third_party::SArray<uint32_t>(worker_ids));
-  for(auto server: local_server){
-    reset_msg.meta.recver=server;
+  for (auto server : local_server) {
+    reset_msg.meta.recver = server;
     sender_->GetMessageQueue()->Push(reset_msg);
   }
   Message reply;
-  while(count>0){
+  while (count > 0) {
     queue.WaitAndPop(&reply);
-    CHECK(reply.meta.flag==Flag::kResetWorkerInModel);
-    CHECK(reply.meta.model_id==table_id);
-    - -count;
+    CHECK(reply.meta.flag == Flag::kResetWorkerInModel);
+    CHECK(reply.meta.model_id == table_id);
+    --count;
   }
-  mailbox_->RegisterQueue(id,&queue);
-  id_mapper_->DeallocateWorkerThread(node_.id,id);
-
-
+  mailbox_->RegisterQueue(id, &queue);
+  id_mapper_->DeallocateWorkerThread(node_.id, id);
 }
 
 void Engine::Run(const MLTask& task) {
   // TODO
   CHECK(task.IsSetup());
-  auto worker_spec =AllocateWorkers(task.GetWorkerAlloc());
-  //Init table
-  const auto& tables =task.GetTables();
-  for(auto& table : tables)
-    InitTable(table,worker_spec.GetAllThreadIds());
-    Barrier();
+  auto worker_spec = AllocateWorkers(task.GetWorkerAlloc());
+  // Init table
+  const auto& tables = task.GetTables();
+  for (auto& table : tables)
+    InitTable(table, worker_spec.GetAllThreadIds());
+  Barrier();
 }
 
 void Engine::RegisterPartitionManager(uint32_t table_id, std::unique_ptr<AbstractPartitionManager> partition_manager) {
   // TODO
+  this->partition_manager_map_.insert(
+      std::map<uint32_t, std::unique_ptr<AbstractPartitionManager>>::value_type(table_id, partition_manager));
 }
 
 }  // namespace csci5570
--- driver/simple_id_mapper.cpp
+++ driver/simple_id_mapper.cpp
@@ -8,82 +8,78 @@
 namespace csci5570 {
 
 SimpleIdMapper::SimpleIdMapper(Node node, const std::vector<Node>& nodes) {
- this->node_=node;
- int i=0;
- while(i<nodes.size()){
-   this->nodes_.push_back(nodes[i]);
-   i++;
+  this->node_ = node;
+  int i = 0;
+  while (i < nodes.size()) {
+    this->nodes_.push_back(nodes[i]);
+    i++;
   }
 }
 
-uint32_t SimpleIdMapper::GetNodeIdForThread(uint32_t tid) {
-  return tid/this->kMaxThreadsPerNode;
-}
+uint32_t SimpleIdMapper::GetNodeIdForThread(uint32_t tid) { return tid / this->kMaxThreadsPerNode; }
 
 void SimpleIdMapper::Init(int num_server_threads_per_node) {
-    int i=0;
-    while(i<nodes_.size()){
-    int n=nodes_[i].id;
+  int i = 0;
+  while (i < nodes_.size()) {
+    int n = nodes_[i].id;
     std::vector<uint32_t> threadid;
-    int j=0;
-    while(j<num_server_threads_per_node){
-      threadid.push_back(n*(this->kMaxThreadsPerNode)+j);
+    int j = 0;
+    while (j < num_server_threads_per_node) {
+      threadid.push_back(n * (this->kMaxThreadsPerNode) + j);
       j++;
     }
-    this->node2server_.insert(std::make_pair(n,threadid));
+    this->node2server_.insert(std::make_pair(n, threadid));
     i++;
   }
 }
 
 uint32_t SimpleIdMapper::AllocateWorkerThread(uint32_t node_id) {
-    int i=0;
-    while(i+this->kMaxBgThreadsPerNode<kMaxThreadsPerNode){
-      if(this->node2worker_[node_id].find(node_id*kMaxThreadsPerNode+i+this->kMaxBgThreadsPerNode)==this->node2worker_[node_id].end()){
-        this->node2worker_[node_id].insert(node_id*kMaxThreadsPerNode+i+this->kMaxBgThreadsPerNode);
-        return node_id*kMaxThreadsPerNode+i+this->kMaxBgThreadsPerNode;
-      }
-      else{
-        i++;
-      }
+  int i = 0;
+  while (i + this->kMaxBgThreadsPerNode < kMaxThreadsPerNode) {
+    if (this->node2worker_[node_id].find(node_id * kMaxThreadsPerNode + i + this->kMaxBgThreadsPerNode) ==
+        this->node2worker_[node_id].end()) {
+      this->node2worker_[node_id].insert(node_id * kMaxThreadsPerNode + i + this->kMaxBgThreadsPerNode);
+      return node_id * kMaxThreadsPerNode + i + this->kMaxBgThreadsPerNode;
+    } else {
+      i++;
     }
-    return -1;
+  }
+  return -1;
 }
 void SimpleIdMapper::DeallocateWorkerThread(uint32_t node_id, uint32_t tid) {
-    if(this->node2worker_[node_id].find(tid)!=this->node2worker_[node_id].end()){
-      this->node2worker_[node_id].erase(tid);
-    }
+  if (this->node2worker_[node_id].find(tid) != this->node2worker_[node_id].end()) {
+    this->node2worker_[node_id].erase(tid);
+  }
 }
 
-std::vector<uint32_t> SimpleIdMapper::GetServerThreadsForId(uint32_t node_id) {
-  return this->node2server_[node_id];
-}
+std::vector<uint32_t> SimpleIdMapper::GetServerThreadsForId(uint32_t node_id) { return this->node2server_[node_id]; }
 std::vector<uint32_t> SimpleIdMapper::GetWorkerHelperThreadsForId(uint32_t node_id) {
-  std::vector<uint32_t> thread_vec=this->node2server_[node_id];
+  std::vector<uint32_t> thread_vec = this->node2server_[node_id];
   std::vector<uint32_t> tmp;
-  int i=0;
-  while(i<thread_vec.size()){
-    tmp.push_back(thread_vec[i]+this->kWorkerHelperThreadId);
+  int i = 0;
+  while (i < thread_vec.size()) {
+    tmp.push_back(thread_vec[i] + this->kWorkerHelperThreadId);
     i++;
   }
   return tmp;
 }
 std::vector<uint32_t> SimpleIdMapper::GetWorkerThreadsForId(uint32_t node_id) {
   std::vector<uint32_t> tmp;
-  std::set<uint32_t>::iterator it=this->node2worker_[node_id].begin();
-  for(;it!=this->node2worker_[node_id].end();it++){
+  std::set<uint32_t>::iterator it = this->node2worker_[node_id].begin();
+  for (; it != this->node2worker_[node_id].end(); it++) {
     tmp.push_back(*it);
   }
   return tmp;
 }
 std::vector<uint32_t> SimpleIdMapper::GetAllServerThreads() {
   std::vector<uint32_t> tmp;
-  int i=0;
-  while(i<this->node2server_.size()){
-      std::vector<uint32_t>::iterator it=this->node2server_[i].begin();
-      for(;it!=this->node2server_[i].end();it++){
-        tmp.push_back(*it);
-      }
+  int i = 0;
+  while (i < this->node2server_.size()) {
+    std::vector<uint32_t>::iterator it = this->node2server_[i].begin();
+    for (; it != this->node2server_[i].end(); it++) {
+      tmp.push_back(*it);
     }
+  }
   return tmp;
 }
 
