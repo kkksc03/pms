02f35da437d9e2d31136d492b9aabae1145b9da3
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-20 15:08:02 +0800

    Data Loader

---
+++ lib/data_loader_test.cpp
@@ -0,0 +1,40 @@
+#include "lib/data_loader.hpp"
+#include <vector>
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+#include "lib/svm_sample.hpp"
+namespace csci5570 {
+
+class TestDataLoader : public testing::Test {
+ public:
+  TestDataLoader() {}
+  ~TestDataLoader() {}
+
+ protected:
+  void SetUp() {}
+  void TearDown() {}
+};
+
+TEST_F(TestDataLoader, LoadData) {
+  using DataStore = std::vector<lib::SVMSample>;
+  using Parser = lib::Parser<lib::SVMSample, DataStore>;
+  // using Parse = int;
+  using Parse = std::function<lib::SVMSample(boost::string_ref, int)>;
+
+  // using Parse=std::function<Sample(boost::string_ref, int)>;
+  DataStore data_store;
+  lib::SVMSample svm_sample;
+  // Parser svm_parser();
+  auto svm_parse = Parser::parse_libsvm;
+  int n_features = 10;
+  std::string url = "hdfs:///datasets/classification/a9";
+  lib::DataLoader<lib::SVMSample, DataStore> data_loader;
+  data_loader.load<Parse>(url, n_features, svm_parse, &data_store);
+  data_loader.test();
+  for (int i = 0; i < data_store.size(); i++) {
+    LOG(INFO) <<"Index :"<<i<<" "<<data_store[i].toString();
+  }
+  LOG(INFO)<<"Size "<<data_store.size();
+}
+
+}  // namespace csci5570
---
+++ lib/svm_sample.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include "base/third_party/sarray.h"
+#include "lib/labeled_sample.hpp"
+namespace csci5570 {
+namespace lib {
+
+// Consider both sparse and dense feature abstraction
+// You may use Eigen::Vector and Eigen::SparseVector template
+
+class SVMSample : public LabeledSample<third_party::SArray<int>, int> {
+public:
+  std::string toString(){
+    std::string result="Label: "+std::to_string(y_)+" Feature:";
+    for (int i = 0; i < x_.size(); i++) {
+        result=result+" "+std::to_string(x_[i]);
+      }
+    return result;
+  }
+  void test() {
+    for (int i = 0; i < 5; i++) {
+      x_.push_back(i);
+    }
+    y_ = 2;
+    
+  }
+  //  public:
+  //   vector<int> x_;
+  //   Label y_;
+};  // class LabeledSample
+
+}  // namespace lib
+}  // namespace csci5570
