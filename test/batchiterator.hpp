8c86fa3fd5b2c78af8221c207b2aa81f28cb5403
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 00:13:45 +0800

    Cancel random

--- test/batchiterator.hpp
+++ test/batchiterator.hpp
@@ -1,36 +1,35 @@
-#include "lib/data_loader.hpp"
-#include "lib/svm_sample.hpp"
-#include "lib/kdd_sample.hpp"
 #include <algorithm>
+#include "lib/data_loader.hpp"
+#include "lib/kdd_sample.hpp"
+#include "lib/svm_sample.hpp"
 
-namespace csci5570{
-    template<typename Sample>
-    class BatchIterator {
-        public:
-        BatchIterator(const std::vector<Sample>& data_store){
-             datastore=data_store;
+namespace csci5570 {
+template <typename Sample>
+class BatchIterator {
+ public:
+  BatchIterator(const std::vector<Sample>& data_store) { datastore = data_store; }
+  std::pair<std::vector<uint32_t>, std::vector<Sample>> NextBatch(uint32_t size) {
+    std::vector<Sample> data;
+    std::vector<uint32_t> keys;
+    for (int i = 0; i < size; i++) {
+      // auto sample=datastore[rand()%datastore.size()];
+      auto sample = datastore[i];
+      data.push_back(sample);
+      auto& x = sample.x_;
+      for (auto& field : x) {
+        int key = field.first;
+        std::vector<uint32_t>::iterator result = find(keys.begin(), keys.end(), key);
+        if (result == keys.end()) {
+          keys.push_back(key);
         }
-        std::pair<std::vector<uint32_t>,std::vector<Sample>> NextBatch(uint32_t size){
-            std::vector<Sample> data;
-            std::vector<uint32_t> keys;
-            for(int i=0;i<size;i++){
-                auto sample=datastore[rand()%datastore.size()];
-                data.push_back(sample);
-                auto& x=sample.x_;
-                for (auto& field : x) {
-                    int key=field.first;
-                    std::vector<uint32_t>::iterator result = find(keys.begin(),keys.end(),key);
-                    if(result==keys.end()){
-                        keys.push_back(key);
-                    }
-                }
-                int n=keys.size();
-                std::sort(keys.begin(),keys.end());
-            }
-            return make_pair(keys,data);
-        }
+      }
+      int n = keys.size();
+      std::sort(keys.begin(), keys.end());
+    }
+    return make_pair(keys, data);
+  }
 
-        private:
-        std::vector<Sample> datastore;
-    };
-}
+ private:
+  std::vector<Sample> datastore;
+};
+}  // namespace csci5570
