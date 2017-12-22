53f37212824eaa6613ce2fc0ed9e8668ba470635
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 09:00:33 +0800

    Change proc_id

--- lib/data_loader.hpp
+++ lib/data_loader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <thread>
 #include <vector>
+#include <algorithm>
 #include "base/serialization.hpp"
 #include "boost/utility/string_ref.hpp"
 #include "io/coordinator.hpp"
@@ -103,7 +104,7 @@ class DataLoader : public AbstractDataLoader<Sample, DataStore> {
     });
 
     // 2. Prepare meta info for the master and workers
-    int proc_id = getpid();  // the actual process id, or you can assign a virtual one, as long as it is distinct
+    int proc_id = rand()%1000;  // the actual process id, or you can assign a virtual one, as long as it is distinct
     std::string master_host = "proj10";  // change to the node you are actually using
     std::string worker_host = "proj10";  // change to the node you are actually using
 
