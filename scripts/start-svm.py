2e4269baa6799b811077acceb16f4fa6811eacb8
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 18:15:26 +0800

    Script update

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -1,4 +1,4 @@
 import os
 for i in range(0,2):
-    cmd="ssh 1155086998@proj"+str(10-i)+" & /data/opt/tmp/1155086998/pms/build/./TestSVM "+str(10-i)+" "+i
+    cmd="ssh 1155086998@proj"+str(10-i)+" & /data/opt/tmp/1155086998/pms/build/./TestSVM "+str(10-i)+" "+str(i)
     os.system(cmd)
