fb44d6edc732303d5f73f5851c8c7ed3c829edc4
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 18:17:10 +0800

    Update script

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -1,4 +1,5 @@
 import os
-for i in range(0,2):
-    cmd="ssh 1155086998@proj"+str(10-i)+" & /data/opt/tmp/1155086998/pms/build/./TestSVM "+str(10-i)+" "+str(i)
+num_of_nodes=2
+for i in range(0,num_of_nodes):
+    cmd="ssh 1155086998@proj"+str(10-i)+" & /data/opt/tmp/1155086998/pms/build/./TestSVM "+str(10-i)+" "+str(num_of_nodes)
     os.system(cmd)
