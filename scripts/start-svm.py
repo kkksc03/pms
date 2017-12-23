fb526615ba63d606f45376b0c4a7d7405a5025db
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-23 17:13:31 +0800

    Port change

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -1,8 +1,8 @@
 import os
 num_of_nodes = 5
 cmd = ""
-port = 12718
-master_port = 32370
+port = 12730
+master_port = 32380
 for i in range(0, num_of_nodes):
     cmd = cmd + "ssh 1155086998@proj" + str(10 - i) + " "
     cmd = cmd + "/data/opt/tmp/1155086998/pms/build/./TestSVM " + \
