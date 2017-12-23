9a0a3d97adec805a5af0dcd7dc66cc23ad723488
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-23 17:30:29 +0800

    Test

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -1,8 +1,8 @@
 import os
 num_of_nodes = 5
 cmd = ""
-port = 12800
-master_port = 32500
+port = 13800
+master_port = 32800
 for i in range(0, num_of_nodes):
     cmd = cmd + "ssh 1155086998@proj" + str(10 - i) + " "
     cmd = cmd + "/data/opt/tmp/1155086998/pms/build/./TestSVM " + \
